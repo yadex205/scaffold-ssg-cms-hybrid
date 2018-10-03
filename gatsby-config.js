d38a9f242300d8dd28a6accffb271e37e20a2f9e
Kanon Kakuno <yadex205@outlook.jp>
2018-10-03 17:59:39 +0900

    :+1: Support SASS with glob import.

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,9 +1,17 @@
+const sassGlobImporter = require('node-sass-glob-importer');
+
 module.exports = {
   siteMetadata: {
     title: 'Gatsby Default Starter',
   },
   plugins: [
     'gatsby-plugin-react-helmet',
-    'gatsby-plugin-typescript'
+    'gatsby-plugin-typescript',
+    {
+      resolve: 'gatsby-plugin-sass',
+      options: {
+        importer: sassGlobImporter()
+      }
+    }
   ]
 }
