8417e93420fa4fae051bee6528cb87d2bd34ea74
Kanon Kakuno <yadex205@outlook.jp>
2018-10-03 15:18:01 +0900

    :+1: Put simple index page.

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,15 +1,5 @@
-import React from 'react'
-import { Link } from 'gatsby'
+import React from 'react';
 
-import Layout from 'components/layout'
-
-const IndexPage = () => (
-  <Layout>
-    <h1>Hi people</h1>
-    <p>Welcome to your new Gatsby site.</p>
-    <p>Now go build something great.</p>
-    <Link to="/page-2/">Go to page 2</Link>
-  </Layout>
-)
-
-export default IndexPage
+export default () => (
+  <div>Hello world!</div>
+);
